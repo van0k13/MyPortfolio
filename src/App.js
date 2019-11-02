7739f56fd151508b4c6a05224dc2e75dfed7010d
Ivan KAZAK <kazak13v@gmail.com>
2019-11-02 22:41:29 +0200

    MyPortfolio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Main from './Main';
+import Skills from './Skills';
+import Works from './Works';
+import Slogan from './Slogan';
+import Contacts from './Contacts';
+import Footer from './Footer';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className="portfolio">
+        <Header />
+        <Main />
+        <Skills />
+        <Works />
+        <Slogan />
+        <Contacts />
+        <Footer />
 
-export default App;
+      </div>
+    </div>
+      );
+    }
+    
+    export default App;
