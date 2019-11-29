e92252d2366485cc577ec90a4f199a4411448867
Ivan KAZAK <kazak13v@gmail.com>
2019-11-29 23:58:38 +0200

    upgraded skills page

--- src/Skills/Main/SkillsMain.js
+++ src/Skills/Main/SkillsMain.js
@@ -8,22 +8,22 @@ function SkillsMain() {
         <div className={styles.skillPhoto1}>
 
         </div>
-        <h2>Speed</h2>
-        <span>skill description</span>
+        <h2 className={styles.textStylization}>React</h2>
+        <span className={styles.textStylization}>vel pharetra vel turpis nunc eget lorem dolor sed viverra ipsum nunc aliquet bibendum enim</span>
       </div>
       <div className={styles.skillName}>
         <div className={styles.skillPhoto2}>
 
         </div>
-        <h2>Strength</h2>
-        <span>skill description</span>
+        <h2 className={styles.textStylization}>{'CSSandJS'}</h2>
+        <span className={styles.textStylization}>vel pharetra vel turpis nunc eget lorem dolor sed viverra ipsum nunc aliquet bibendum enim</span>
       </div>
       <div className={styles.skillName}>
         <div className={styles.skillPhoto3}>
 
         </div>
-        <h2>Guitarability</h2>
-        <span>skill description</span>
+        <h2 className={styles.textStylization}>HTMLandJSX</h2>
+        <span className={styles.textStylization}>vel pharetra vel turpis nunc eget lorem dolor sed viverra ipsum nunc aliquet bibendum enim</span>
       </div>
     </div>
   );
